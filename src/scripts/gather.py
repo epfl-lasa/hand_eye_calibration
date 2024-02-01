212e6128cdcdb9c50e9dafd6b12a633937b597a1
Brunoadc <bruno.agostinhodacosta@epfl.ch>
2024-02-01 11:32:47 +0100

    Images for readme and first version of gather and proces for the dual estimation (camera asset and base asset)

--- src/scripts/gather.py
+++ src/scripts/gather.py
@@ -5,20 +5,39 @@ import rospy
 import transforms3d
 import numpy as np
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Bool
 
 # Global variables to store the latest data
-latest_cam_grasp_pose = None
-latest_aruco_pose = None
+latest_cam_pose = None
+latest_robot_base_pose = None
+latest_marker_pose = None
+latest_ee_pose = None
+gather = None
 
-# Callback for the /vrpn_client_node/cam_grasp/pose_transform topic
-def cam_grasp_callback(msg):
-    global latest_cam_grasp_pose
-    latest_cam_grasp_pose = msg
+# Callback for the camera topic
+def cam_callback(msg):
+    global latest_cam_pose
+    latest_cam_pose = msg
+    
+# Callback for the robot base topic
+def robot_base_callback(msg):
+    global latest_robot_base_pose
+    latest_robot_base_pose = msg
 
-# Callback for the aruco_single/pose topic
-def aruco_callback(msg):
-    global latest_aruco_pose
-    latest_aruco_pose = msg
+# Callback for the marker pose
+def marker_callback(msg):
+    global latest_marker_pose
+    latest_marker_pose = msg
+
+# Callback for the marker pose
+def ee_pose_callback(msg):
+    global latest_ee_pose
+    latest_ee_pose = msg
+
+# Boolean callback to start gathering pose data (robot reached correct pose) 
+def gathering_callback(msg):
+    global gather 
+    gather = msg.data
 
 def posestamped_to_so4(pose_stamped, inverse_transformation=False):
     """
@@ -59,51 +78,74 @@ def posestamped_to_so4(pose_stamped, inverse_transformation=False):
     return transformation_matrix
 
 def main():
+    global latest_cam_pose, latest_robot_base_pose, latest_marker_pose, latest_ee_pose, gather
     # Initialize the ROS Node
     rospy.init_node('pose_subscriber', anonymous=True)
 
     # Subscribe to the topics
-    rospy.Subscriber("/vrpn_client_node/cam_grasp/pose", PoseStamped, cam_grasp_callback)
-    rospy.Subscriber("aruco_single/pose", PoseStamped, aruco_callback)
+    rospy.Subscriber("/vrpn_client_node/cam_grasp/pose", PoseStamped, cam_callback)
+    rospy.Subscriber("/vrpn_client_node/franka_base16/pose", PoseStamped, robot_base_callback)
+    rospy.Subscriber("aruco_single/pose", PoseStamped, marker_callback)
+    rospy.Subscriber("ee_pose", PoseStamped, ee_pose_callback)
+    rospy.Subscriber('gathering', Bool, gathering_callback)
 
-    n = 20 #1+2*6 # First centered then pos/neg translation/rotation for each axis
-    A = np.zeros((4,4,n))
-    B = np.zeros((4,4,n))
+    gathering_pub = rospy.Publisher('position_gathered', Bool, queue_size=1)
+
+    n = 64 # depends on the number of poses in the control algorithm
+    A1 = np.zeros((4,4,n))
+    B1 = np.zeros((4,4,n))
+    A2 = np.zeros((4,4,n))
+    B2 = np.zeros((4,4,n))
     
     i = 0
     
+    time_threshold = 0.5 #Ensures that we are still receiving data from optitrack
     
     # Main loop
     try:
         while not rospy.is_shutdown() and i < n:
-            input("Press Enter to save the current poses...")
-            print("")
-            
-            # Store and print the latest data
-            if latest_cam_grasp_pose and latest_aruco_pose:
-                now = rospy.Time.now().to_sec()
-                delta_time_optitrack = latest_cam_grasp_pose.header.stamp.to_sec() - now
-                delta_time_marker = latest_aruco_pose.header.stamp.to_sec() - now
-                if delta_time_optitrack < 0.5 and delta_time_marker < 0.5:
-                    A[:,:,i]= posestamped_to_so4(latest_cam_grasp_pose)
-                    B[:,:,i]= posestamped_to_so4(latest_aruco_pose, inverse_transformation = True)
-                    i+=1
-                else:
-                    if delta_time_optitrack > 0.5:
-                        print("No data from optitrack since [s]: " +str(delta_time_optitrack))
-                    if delta_time_marker > 0.5:
-                        print("No data from marker since [s]: " +str(delta_time_optitrack))
+            if gather:
+                rospy.sleep(2) #wait for arm to stop moving
+                data_captured = False
+                # Store and print the latest data
+                if latest_cam_pose and latest_robot_base_pose and latest_marker_pose:
+                    now = rospy.Time.now().to_sec()
+                    delta_time_camera = latest_cam_pose.header.stamp.to_sec() - now
+                    delta_time_robot_base = latest_robot_base_pose.header.stamp.to_sec() - now
+                    delta_time_marker = latest_marker_pose.header.stamp.to_sec() - now
+                    
+                    if np.max(delta_time_camera, delta_time_robot_base, delta_time_marker) < 0.5:
+                        A1[:,:,i] = posestamped_to_so4(latest_cam_pose)
+                        B1[:,:,i] = posestamped_to_so4(latest_marker_pose, inverse_transformation = True)
                         
-            else:
-                if latest_cam_grasp_pose:
-                    print("No data received from marker pose")
-                elif latest_aruco_pose:
-                    print("No data received from optitrack for camera pose")
+                        A2[:,:,i] = posestamped_to_so4(latest_cam_pose, inverse_transformation) @ posestamped_to_so4(latest_robot_base_pose)
+                        B2[:,:,i] = posestamped_to_so4(latest_ee_pose, inverse_transformation)
+                        
+                        i+=1
+                        data_captured = True
+                    else:
+                        if delta_time_camera > 0.5:
+                            rospy.logerr("No data from camera pose since [s]: " +str(delta_time_camera))
+                        if delta_time_robot_base > 0.5:
+                            rospy.logerr("No data from robot base pose since [s]: " +str(delta_time_camera))
+                        if delta_time_marker > 0.5:
+                            rospy.logerr("No data from marker pose since [s]: " +str(delta_time_camera))
+                            
                 else:
-                    print("No data received")
-            
-            print("Picture " +str(i) + "/" + str(n))
-            rospy.sleep(1) 
+                    if latest_cam_pose:
+                        rospy.logerr("No data received from marker pose topic")
+                    if latest_robot_base_pose:
+                        rospy.logerr("No data received from robot pose topic")
+                    if latest_marker_pose:
+                        rospy.logerr("No data received from camera pose topic")
+                
+                if data_captured:
+                    gathering_pub.publish(True)
+                    gather = False
+                    rospy.loginfo("Picture " +str(i) + "/" + str(n))
+                    
+
+            rospy.sleep(1)
             
                     
 
@@ -114,8 +156,10 @@ def main():
     script_dir = os.path.dirname(os.path.realpath(__file__))
     
     # Save the A and B matrices to .npy files in the same directory as the script
-    np.save(os.path.join(script_dir, 'A_matrices.npy'), A)
-    np.save(os.path.join(script_dir, 'B_matrices.npy'), B)
+    np.save(os.path.join(script_dir, 'A1_matrices.npy'), A1)
+    np.save(os.path.join(script_dir, 'B1_matrices.npy'), B1)
+    np.save(os.path.join(script_dir, 'A2_matrices.npy'), A2)
+    np.save(os.path.join(script_dir, 'B2_matrices.npy'), B2)
 
 
 if __name__ == '__main__':
--- src/scripts/process.py
+++ src/scripts/process.py
@@ -10,11 +10,11 @@ rel_path = "../Pose-Estimation-for-Sensor-Calibration"
 abs_file_path = os.path.join(script_dir, rel_path)
 sys.path.append(abs_file_path)
 
-from Pose_Estimation_Class import Batch_Processing
+from Pose_Estimation_Class import Batch_Processing, EKF, IEKF, UKF
 
-def so4_to_posestamped(transformation_matrix):
+def se3_to_posestamped(transformation_matrix):
     """
-    Convert a 4x4 transformation matrix (SO(4)) to a PoseStamped message.
+    Convert a 4x4 transformation matrix (SE(3)) to a PoseStamped message.
 
     Args:
     transformation_matrix (numpy.ndarray): 4x4 transformation matrix.
@@ -45,25 +45,63 @@ def so4_to_posestamped(transformation_matrix):
 
     return pose_stamped
 
+def result_to_se3():
+    return 0
+
+
 def main():
     # Determine the path for loading the .npy files
     script_dir = os.path.dirname(os.path.realpath(__file__))
     
     # Load the A and B matrices from .npy files in the same directory as the script
-    A = np.load(os.path.join(script_dir, 'A_matrices.npy'))
-    B = np.load(os.path.join(script_dir, 'B_matrices.npy'))
+    A1 = np.load(os.path.join(script_dir, 'A1_matrices.npy'))
+    B1 = np.load(os.path.join(script_dir, 'B1_matrices.npy'))
+    A2 = np.load(os.path.join(script_dir, 'A2_matrices.npy'))
+    B2 = np.load(os.path.join(script_dir, 'B2_matrices.npy'))
+    n = A.shape[2]//2
 
+    #Iterate over the matrices two by two
+    AA1 = np.empty((4, 4, n))
+    BB1 = np.empty((4, 4, n))
+    AA2 = np.empty((4, 4, n))
+    BB2 = np.empty((4, 4, n))
+    for i in range(n):
+        # Multiply pairs of matrices
+        AA1[:, :, i] = A1[:, :, 2*i] @ np.linalg.inv(A1[:, :, 2*i + 1])
+        BB1[:, :, i] = B1[:, :, 2*i] @ np.linalg.inv(B1[:, :, 2*i + 1])
+        AA2[:, :, i] = A2[:, :, 2*i] @ np.linalg.inv(A2[:, :, 2*i + 1])
+        BB2[:, :, i] = B2[:, :, 2*i] @ np.linalg.inv(B2[:, :, 2*i + 1])
+
+
+    # Senidng data to the Kalman Filtering methods
+    ekf1=EKF()
+    ekf2=EKF()
+    iekf1=IEKF()
+    iekf2=IEKF()
+    ukf1=UKF()
+    ukf2=UKF()
+    for i in range(n):
+        ekf1.Update(AA1,BB1)
+        iekf1.Update(AA1,BB1)
+        ukf1.Update(AA1,BB1)
+        ekf2.Update(AA2,BB2)
+        iekf2.Update(AA2,BB2)
+        ukf2.Update(AA2,BB2)
+        
+    
     # Perform pose estimation
-    X_est,Y_est,Y_est_check,ErrorStats = Batch_Processing.pose_estimation(A, B)
+    X_est_batch,Y_est_batch,Y_est_check_batch,ErrorStats_batch = Batch_Processing.pose_estimation(A1, B1)
+    
+    
 
     # Print the results
     # Set the numpy print options
     np.set_printoptions(precision=6, suppress=True)
     print('\n.....Batch Processing Results\n')
-    print("Y Pose: \n" + str(so4_to_posestamped(Y_est)))
-    print("\nX Pose: \n" + str(so4_to_posestamped(X_est)))
-    print("\nX Matrix: \n" + str(X_est))
-    print("\nErrorStats: " + str(ErrorStats))
+    print("Y Pose: \n" + str(se3_to_posestamped(Y_est_batch)))
+    print("\nX Pose: \n" + str(se3_to_posestamped(X_est_batch)))
+    print("\nX Matrix: \n" + str(X_est_batch))
+    print("\nErrorStats: " + str(ErrorStats_batch))
     print("")
 
 if __name__ == '__main__':
